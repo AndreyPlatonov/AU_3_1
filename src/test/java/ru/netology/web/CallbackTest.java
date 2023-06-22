bf3b684dd00ea49fc5df22f61f1661ec3c1e73a1
AndreyPlatonov <one.and.pl@gmail.com>
2023-06-23 00:33:14 +0300

    failed

---
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -0,0 +1,23 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CallbackTest {
+
+    @Test
+    void shouldSubmitRequest() {
+        open("http://localhost:9999/");
+        SelenideElement form = $("[data-test-id=callback-form]");
+        form.$("[data-test-id=name] input").setValue("Василий");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[data-test-id=submit]").click();
+        $(".alert-success").shouldHave(Condition.exactText("Ваша заявка успешно отправлена!"));
+
+    }
+}
