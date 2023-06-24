8720074e6d16c86603c26137ee561c43e9c73e4a
AndreyPlatonov <one.and.pl@gmail.com>
2023-06-25 00:02:33 +0300

    fix DZ

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -2,6 +2,7 @@ package ru.netology.web;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -9,10 +10,14 @@ import java.util.List;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CallbackTest {
+    @BeforeEach
+    void SetUp() {
+        open("http://localhost:9999/");
+    }
 
     @Test
     void shouldSubmitSuccessRequest() {
-        open("http://localhost:9999/");
+
         SelenideElement form = $(".form_theme_alfa-on-white");
         form.$("[data-test-id=name] input").setValue("Василий");
         form.$("[data-test-id=phone] input").setValue("+79270000000");
@@ -24,50 +29,50 @@ public class CallbackTest {
 
     @Test
     void shouldSubmitNullName() {
-        open("http://localhost:9999/");
+
         SelenideElement form = $(".form_theme_alfa-on-white");
+        form.$("[data-test-id=name] input").setValue("");
+        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        List<SelenideElement> subs = $$(".input__sub");
-        subs.get(0).shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
     }
 
     @Test
     void shouldSubmitWrongName() {
-        open("http://localhost:9999/");
+
         SelenideElement form = $(".form_theme_alfa-on-white");
         form.$("[data-test-id=name] input").setValue("Василий22");
         form.$("[data-test-id=phone] input").setValue("+79270000000");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        List<SelenideElement> subs = $$(".input__sub");
-        subs.get(0).shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $("[data-test-id=name].input_invalid .input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
     void shouldSubmitNullPhone() {
-        open("http://localhost:9999/");
+
         SelenideElement form = $(".form_theme_alfa-on-white");
         form.$("[data-test-id=name] input").setValue("Василий иванов");
+        form.$("[data-test-id=phone] input").setValue("");
         form.$(".button").click();
-        List<SelenideElement> subs = $$(".input__sub");
-        subs.get(1).shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
     }
 
     @Test
     void shouldSubmitWrongPhone() {
-        open("http://localhost:9999/");
+
         SelenideElement form = $(".form_theme_alfa-on-white");
         form.$("[data-test-id=name] input").setValue("Василий иванов");
         form.$("[data-test-id=phone] input").setValue("222222");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        List<SelenideElement> subs = $$(".input__sub");
-        subs.get(1).shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $("[data-test-id=phone].input_invalid .input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
     void shouldSubmitNoCheck() {
-        open("http://localhost:9999/");
+
         SelenideElement form = $(".form_theme_alfa-on-white");
         form.$("[data-test-id=name] input").setValue("Василий иванов");
         form.$("[data-test-id=phone] input").setValue("+79270000000");
